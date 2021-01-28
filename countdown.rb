5faf85743260f7ff791a9f74faecc4711a48a15d
Justin Movitz <movitzj1@montclair.edu>
2021-01-28 02:07:32 -0500

    commit tree 84095322108993934881f21ca50bdbc08dd98e2d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+def countdown(num)
+    while num > 0 do
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+    while num > 0 do
+        puts "#{num} SECOND(S)!"
+        sleep 1
+        num -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+#puts countdown_with_sleep(10)
+#puts countdown(10)
